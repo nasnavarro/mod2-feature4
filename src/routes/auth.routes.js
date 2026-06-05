a3112affb7cce91f76116c34aa9546529e042137
Ignacio <inavarro@tesoriza.com>
2026-06-05 20:53:12 +0200

    Se añade un rateLimit a la ruta del login, permitiendo sólo 3 intentos por minuto. FIN DEL PROYECTO.

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,14 @@
 import { Router } from "express";
+import { rateLimit } from "express-rate-limit";
 import * as authController from "../controllers/auth.controller.js";
+
+// Configura un limitador de peticiones específico para la ruta
+// de login, permitiendo solo 3 intentos por minuto
+const loginLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 3,
+  message: { ok: false, error: { message: 'Demasiados intentos de login, espera 1 minuto' } },
+});
 
 const router = Router();
 
@@ -7,6 +16,6 @@ const router = Router();
 // index.routes: /api/auth
 
 router.post('/register', authController.register);
-router.post('/login', authController.login);
+router.post('/login', loginLimiter, authController.login);
 
 export default router;
